d25a468a575d79cd511d607a2a82b13a3b57b4aa
Anik Hasan <55820217+AnikHasan-web@users.noreply.github.com>
2019-09-27 11:21:39 +0600

    Day-3

---
+++ prob-5 Hashtable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable nameList = new Hashtable();
+            nameList.Add(1, "anik");
+            nameList.Add(2, "ashik");
+            nameList.Add(3, "amit");
+
+            foreach(var Key in nameList.Keys)
+            {
+                Console.WriteLine(nameList[Key]);
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
