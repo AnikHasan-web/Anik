fad5a65c5a8f97b15b457b441981d78b2476809d
Anik Hasan <55820217+AnikHasan-web@users.noreply.github.com>
2019-10-02 18:43:53 +0600

    day-5

---
+++ save Name inarray/day-5 array name/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day_5
+{
+    public partial class NameForm : Form
+    {
+        private List<string> namelist = new List<string>();
+
+            
+          
+        public NameForm()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            namelist.Add(nameTextBox.Text);
+            CLear();
+
+        }
+
+
+        private void CLear()
+        {
+            nameTextBox.Clear();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            foreach (string name in namelist)
+            {
+                nameListBox.Items.Add(name);
+            }
+
+            countTextBox.Text = Convert.ToString(namelist.Count());
+        }
+       
+    }
+}
