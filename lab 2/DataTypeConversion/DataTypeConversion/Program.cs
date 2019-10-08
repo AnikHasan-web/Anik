f6539e77d280865b151e6f79ef222a1c2b79499e
Anik Hasan <55820217+AnikHasan-web@users.noreply.github.com>
2019-10-08 20:56:27 +0600

    Add files via upload

---
+++ lab 2/DataTypeConversion/DataTypeConversion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = 0;
+            long l = 4654648;
+
+            l = numberOne; // Implicit Conversion 
+
+            numberOne = Convert.ToInt32(l); // Explicit Converstion 
+
+            //bool a  = false;
+
+            //Console.WriteLine(Convert.ToInt32(a));
+
+            char c = 'a';
+
+            string s = "8";
+
+            Console.WriteLine(Convert.ToInt32(s));
+
+            Console.ReadKey();
+        }
+    }
+}
