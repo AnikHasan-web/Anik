af1673579f9ea6d14f9bf4396175a8330000c9d4
Anik Hasan <55820217+AnikHasan-web@users.noreply.github.com>
2019-10-08 20:40:43 +0600

    Add files via upload

---
+++ lab 2/New folder/method challenge/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodChallangeSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fName = Console.ReadLine();
+            string mName = Console.ReadLine();
+            string lName = Console.ReadLine();
+
+            string FullName = GetFullName(fName, mName, lName);
+
+            Console.WriteLine(FullName);
+
+            xyz(5);
+        }
+
+        static string GetFullName(string firstName, string middleName, string lastName)
+        {
+
+            string FullName = firstName+" " + middleName + " " + lastName;
+            int age = 30;
+
+            return FullName;
+
+        }
+
+        static string xyz(int i)
+        {
+            if (i >= 40)
+            {
+                return "Passed";
+            }
+
+            return "Failed";
+        }
+    }
+}
---
+++ lab 2/New folder/while loop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0 ;
+           while(true)
+            {
+                i++;
+                if (i == 100)
+                {
+                    break;
+                }
+                if (i%2==0)
+                {
+
+                    
+                    continue;
+                }
+               
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
