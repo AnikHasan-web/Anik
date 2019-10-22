6fba6fd882d5c04a57de5a02b89f85e464978e60
Anik Hasan <55820217+AnikHasan-web@users.noreply.github.com>
2019-10-22 12:21:34 +0600

    Add files via upload

---
+++ lab 12/BankAccountPractice/OopUpdate/OopUpdate/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopUpdate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Anik", "Hasan");
+
+            //GetChange(person1);
+            string fullName = person1.GetFullName();
+            string reverseName = person1.GetReverseName();
+            person1.personalId = "12345";
+            Console.WriteLine("Personal Id: " + person1.personalId);
+            Console.WriteLine("Full Name: " + fullName);
+            Console.WriteLine("Reverse Name: " + reverseName + "\n");
+            
+
+
+            Console.WriteLine("--------------------------------------" + "\n");
+
+
+            Person person2 = new Person("Md", "Adorsha", "Hasan");
+            
+            fullName = person2.GetFullName();
+            reverseName = person2.GetReverseName();
+            person2.personalId = "67890";
+            Console.WriteLine("Personal Id: " + person2.personalId);
+            Console.WriteLine("Full Name: " + fullName);
+            Console.WriteLine("Reverse Name: " + reverseName + "\n");
+            
+
+            Console.WriteLine("--------------------------------------" + "\n");
+
+            Person person3 = person2;
+
+            person2.LastName = "Hossain";
+            fullName = person2.GetFullName();
+            reverseName = person2.GetReverseName();
+            person3.personalId = "111111";
+            Console.WriteLine("Personal Id: " + person3.personalId);
+            Console.WriteLine("Full Name: " + fullName);
+            Console.WriteLine("Reverse Name: " + reverseName);
+            
+
+            Console.WriteLine("--------------------------------------" + "\n");
+
+            Console.ReadKey();
+
+        }
+
+        //static void GetChange(Person person)
+        //{
+        //    person.LastName = "Changed";
+        //}
+    }
+}
---
+++ lab 12/BankAccountPractice/OopUpdate/OopUpdate/person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopUpdate
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string personalId;
+
+        
+        public Person(string firstName, string middleName, string lastName)
+            : this(firstName, lastName)
+        {
+            this.MiddleName = middleName;
+        }
+
+        public Person(string firstName, string lastName)
+            : this()
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public Person()
+        {
+
+        }
+
+        public string GetFullName()
+        {
+            string fullName = FirstName + " " + MiddleName + " " + LastName;
+            return fullName;
+        }
+
+        public string GetReverseName()
+        {
+            string fullName = GetFullName();
+            char[] charArray = fullName.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
