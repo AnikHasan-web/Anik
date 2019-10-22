d5b9936d9f2aba83e14894d84f1488d556eed7c5
Anik Hasan <55820217+AnikHasan-web@users.noreply.github.com>
2019-10-22 12:22:51 +0600

    Add files via upload

---
+++ lab 12/BankAccountPractice/Oopupdate 1/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopBasic
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Id { get; set; }
+        
+
+       
+        public string GetFullName()
+        {
+            if (MiddleName == null)
+            {
+                return FirstName + " " + LastName;
+            }
+            else
+            {
+                return FirstName + " " + MiddleName + " " + LastName;
+            }
+           
+           
+        }
+        
+
+        public string GetReverseName()//method
+        {
+            string fullName = GetFullName();
+            char[] charArray = fullName.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
