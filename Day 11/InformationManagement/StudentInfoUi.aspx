bc088cb23fc50d9194215daa27008ee43c04cb75
Anik Hasan <55820217+AnikHasan-web@users.noreply.github.com>
2019-10-24 11:54:50 +0600

    Add files via upload

---
+++ Day 11/InformationManagement/StudentInfoUi.aspx
@@ -0,0 +1,80 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="StudentInfoUi.aspx.cs" Inherits="InformationManagement.StudentInfoUi" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Student Information Form</h2>
+    <div class="form-group">
+        <label for="inputStudentName">Student Name</label>
+        <input type="text" class="form-control" id="inputStudentName" runat="server" placeholder="Enter Name"/>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" Display="Dynamic" ControlToValidate="inputStudentName" ForeColor="red" ErrorMessage="Name Can't be empty"></asp:RequiredFieldValidator>
+    </div>
+    <div class="form-group">
+        <label for="inputRegNo">Student Reg No</label>
+        <input type="text" class="form-control" runat="server" id="inputRegNo" placeholder="Enter Reg No"/>
+    </div>
+    <div class="form-group">
+        <label for="inputEmailAddress">Email Address</label>
+        <input type="text" runat="server" class="form-control" id="inputEmailAddress" placeholder="Enter Email"/>
+    </div>
+    <div class="form-group">
+        <label for="inputMobileNo">Mobile No</label>
+        <input type="text" runat="server" class="form-control" id="inputMobileNo" placeholder="Enter Mobile No"/>
+    </div>
+    <div class="form-group">
+        <label for="inputAge">Age</label>
+        <input type="text" class="form-control" runat="server" id="inputAge" placeholder="Enter Age"/>
+        <asp:CompareValidator ID="CompareValidator1" runat="server" ForeColor="red" ControlToValidate="inputAge" Display="Dynamic" Operator="DataTypeCheck" Type="Integer" ErrorMessage="Invalid Input"></asp:CompareValidator>
+    </div>
+    <div class="form-group">
+        <label for="inputAddess">Address</label>
+        <input type="text" runat="server" class="form-control" id="inputAddess" placeholder="Enter Address"/>
+    </div>
+    <asp:Button ID="saveButton" class="btn btn-primary" runat="server" Text="Save" OnClick="saveButton_Click" />
+    <br/>
+    <asp:Label ID="messageLabel" runat="server"></asp:Label>
+    <br />
+    <br />
+    <asp:GridView ID="StudentGrirdView" runat="server" AutoGenerateColumns="False" CellPadding="4" ForeColor="#333333" GridLines="None">
+        <AlternatingRowStyle BackColor="White" />
+        <Columns>
+            <asp:TemplateField HeaderText="Student Name">
+                <ItemTemplate>
+                    <%#Eval("StudentName") %>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Reg No">
+                <ItemTemplate>
+                    <%#Eval("RegNo") %>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Email Address">
+                <ItemTemplate>
+                    <%#Eval("EmailAddress") %>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Mobile No">
+                <ItemTemplate>
+                    <%#Eval("MobileNo") %>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Age">
+                <ItemTemplate>
+                    <%#Eval("Age") %>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Address">
+                <ItemTemplate>
+                    <%#Eval("Address") %>
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+        <EditRowStyle BackColor="#2461BF" />
+        <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+        <RowStyle BackColor="#EFF3FB" />
+        <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+        <SortedAscendingCellStyle BackColor="#F5F7FB" />
+        <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+        <SortedDescendingCellStyle BackColor="#E9EBEF" />
+        <SortedDescendingHeaderStyle BackColor="#4870BE" />
+    </asp:GridView>
+</asp:Content>
---
+++ Day 11/InformationManagement/StudentInfoUi.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using InformationManagement.Models;
+
+namespace InformationManagement
+{
+    public partial class StudentInfoUi : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ShowAllStudentInfo();
+        }
+
+       
+
+        protected void saveButton_Click(object sender, EventArgs e)
+        {
+            string name = inputStudentName.Value;
+            string regNo = inputRegNo.Value;
+            string email = inputEmailAddress.Value;
+            string mobileNo = inputMobileNo.Value;
+            int age = Convert.ToInt32(inputAge.Value);
+            string address = inputAddess.Value;
+            Student student = new Student(name, regNo, email, mobileNo, age, address);
+
+            string connectionString =
+                @"Server = BITM-TRAINER-30\SQLEXPRESS;Database = InformationManagementDB;Integrated Security=true;";
+            var rowEffect = InsertStudentInfo(connectionString, student);
+
+            if (rowEffect > 0)
+            {
+                messageLabel.Text = String.Empty;
+                messageLabel.Text = "Student Information Saved";
+                messageLabel.ForeColor = Color.Green;
+                ClearField();
+                ShowAllStudentInfo();
+            }
+            else
+            {
+                messageLabel.Text = String.Empty;
+                messageLabel.Text = "Failed to save";
+                messageLabel.ForeColor = Color.Red;
+            }
+
+        }
+
+        private int InsertStudentInfo(string connectionString, Student student)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string query = "INSERT INTO " +
+                           "Student_tbl(StudentName, RegNo," +
+                           " Email, MobileNo, Age, Address)" +
+                           "VALUES('" + student.StudentName + "','" + student.RegNo + "'," +
+                           "'" + student.EmailAddress + "','" + student.MobileNo + "'," +
+                           "" + student.Age + ",'" + student.Address + "')";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            int rowEffect = command.ExecuteNonQuery();
+            connection.Close();
+            return rowEffect;
+        }
+
+        private void ShowAllStudentInfo()
+        {
+            List<Student> studentList = new List<Student>();
+            string connectionString =
+                @"Server = BITM-TRAINER-30\SQLEXPRESS;Database = InformationManagementDB;Integrated Security=true;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = "SELECT * FROM Student_tbl";
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                int studentId = (int)reader["StudentId"];
+                string studentName = reader["StudentName"].ToString();
+                string regNo = reader["RegNo"].ToString();
+                string email = reader["Email"].ToString();
+                string mobileNo = reader["MobileNo"].ToString();               
+                int age = (int)reader["Age"];
+                string address = reader["Address"].ToString();
+                Student student = new Student(studentName,regNo,email,mobileNo,age,address);
+                student.StudentId = studentId;
+                studentList.Add(student);
+            }
+            reader.Close();
+            connection.Close();
+            StudentGrirdView.DataSource = studentList;
+            StudentGrirdView.DataBind();
+
+        }
+        protected void ClearField()
+        {
+            inputStudentName.Value=String.Empty;
+            inputRegNo.Value=String.Empty;
+            inputEmailAddress.Value=String.Empty;
+            inputMobileNo.Value=String.Empty;
+            inputAge.Value=String.Empty;
+            inputAddess.Value=String.Empty;
+        }
+    }
+}
