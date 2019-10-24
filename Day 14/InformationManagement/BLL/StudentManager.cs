0f94adf60995874c3c322e93e8244e0786246e0f
Anik Hasan <55820217+AnikHasan-web@users.noreply.github.com>
2019-10-24 12:08:45 +0600

    Add files via upload

---
+++ Day 14/InformationManagement/BLL/StudentManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using InformationManagement.DAL;
+using InformationManagement.Models;
+
+namespace InformationManagement.BLL
+{
+    public class StudentManager
+    {
+        public  string SaveStudent(Student student)
+        {
+            StudentGateway gateway=new StudentGateway();
+            Student aStudent = gateway.GetExistStudent(student);
+            if (aStudent != null)
+            {
+                return "Registration number already exist.Please enter unique reg no";
+            }
+
+            bool isSaved = gateway.SaveStudent(student);
+            if (isSaved)
+            {
+                return "Student saved successfully";
+            }
+            else
+            {
+                return "Save Failed";
+            }
+
+
+        }
+    }
+}
