2d2e7b73957d5f227d4be61d73d1d16890b65f4a
Anik Hasan <55820217+AnikHasan-web@users.noreply.github.com>
2019-10-24 12:05:27 +0600

    Add files via upload

---
+++ Day 14/InformationManagement/StudentInfoUi.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using InformationManagement.BLL;
+using InformationManagement.Models;
+
+namespace InformationManagement
+{
+    public partial class StudentInfoUi : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ShowAllStudentInfo();
+        }
+
+       
+
+        protected void saveButton_Click(object sender, EventArgs e)
+        {
+            StudentManager manager=new StudentManager();
+            string name = inputStudentName.Value;
+            string regNo = inputRegNo.Value;
+            string email = inputEmailAddress.Value;
+            string mobileNo = inputMobileNo.Value;
+            int age = Convert.ToInt32(inputAge.Value);
+            string address = inputAddess.Value;
+            Student student = new Student(name, regNo, email, mobileNo, age, address);
+
+            messageLabel.Text = manager.SaveStudent(student);
+
+        }
+
+       
+
+        private void ShowAllStudentInfo()
+        {
+            List<Student> studentList = new List<Student>();
+            string connectionString =WebConfigurationManager.ConnectionStrings["IMDB"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = "SELECT * FROM Student_tbl";;
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                int studentId = (int)reader["StudentId"];
+                string studentName = reader["StudentName"].ToString();
+                string regNo = reader["RegNo"].ToString();
+                string email = reader["Email"].ToString();
+                string mobileNo = reader["MobileNo"].ToString();               
+                int age = (int)reader["Age"];
+                string address = reader["Address"].ToString();
+                Student student = new Student(studentName,regNo,email,mobileNo,age,address);
+                student.StudentId = studentId;
+                studentList.Add(student);
+            }
+            reader.Close();
+            connection.Close();
+            StudentGrirdView.DataSource = studentList;
+            StudentGrirdView.DataBind();
+
+        }
+        protected void ClearField()
+        {
+            inputStudentName.Value=String.Empty;
+            inputRegNo.Value=String.Empty;
+            inputEmailAddress.Value=String.Empty;
+            inputMobileNo.Value=String.Empty;
+            inputAge.Value=String.Empty;
+            inputAddess.Value=String.Empty;
+        }
+    }
+}
