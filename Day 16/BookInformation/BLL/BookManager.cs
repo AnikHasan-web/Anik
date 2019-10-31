d82cb5cba78d2d8f5422f6e175102b73612a619f
Anik Hasan <55820217+AnikHasan-web@users.noreply.github.com>
2019-10-31 09:26:30 +0600

    Add files via upload

---
+++ Day 16/BookInformation/BLL/BookManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BookInformation.DAL;
+using BookInformation.Model;
+
+namespace BookInformation.BLL
+{
+    public class BookManager
+    {
+        BookGateway bookGateway = new BookGateway();
+
+        public string SaveBook(Book book)
+        {
+           if(book.Isbn.Length == 13) { 
+               int rowEffect = bookGateway.SaveBook(book);
+               if (rowEffect>0)
+               {
+                 return "Saved Successfully";
+               }
+               else
+               {
+                   return "Failed to Save";
+               }
+               }
+           else
+           {
+               return "Isbn must be 13 Characters long";
+           }
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return bookGateway.GetAllBook();
+        }
+    }
+}
