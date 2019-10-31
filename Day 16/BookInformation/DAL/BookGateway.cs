73548e66df07156384a08ed1940c14292b4d02a2
Anik Hasan <55820217+AnikHasan-web@users.noreply.github.com>
2019-10-31 09:23:12 +0600

    Add files via upload

---
+++ Day 16/BookInformation/DAL/BookGateway.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using BookInformation.Model;
+
+namespace BookInformation.DAL
+{
+    public class BookGateway
+    {
+        public int SaveBook(Book book)
+        {
+            string connectionString =
+                @"Server = PC-301-17\SQLEXPRESS; Database = BookInformationKeeperDB; Integrated Security = true";
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = "INSERT INTO Book_tbl Values('" + book.Name + "','" + book.Isbn + "','" + book.Author + "')";
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            int rowEffect = command.ExecuteNonQuery();
+            connection.Close();
+            return rowEffect;
+        }
+
+        public List<Book> GetAllBook()
+        {
+            string connectionString =
+                @"Server = PC-301-17\SQLEXPRESS; Database = BookInformationKeeperDB; Integrated Security = true";
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = "SELECT * FROM Book_tbl";
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            List<Book> books = new List<Book>();
+            while (reader.Read())
+            {
+                Book book =new Book();
+                book.BookId = (int) reader["BookId"];
+                book.Name = reader["BookName"].ToString();
+                book.Isbn = reader["Isbn"].ToString();
+                book.Author = reader["Author"].ToString();
+                books.Add(book);
+               
+            }
+            reader.Close();
+            connection.Close();
+
+            return books;
+        }
+    }
+}
---
+++ Day 16/BookInformation/Model/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookInformation.Model
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public string Isbn { get; set; }
+        public string Author { get; set; }
+        public int BookId { get; internal set; }
+
+        public Book(string name, string isbn, string author)
+        {
+            Name = name;
+            Isbn = isbn;
+            Author = author;
+        }
+
+        public Book()
+        {
+
+        }
+    }
+}
